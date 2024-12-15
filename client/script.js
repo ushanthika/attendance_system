2a80b600b26e46581df4a42e3944574665a62e1d
ushanthika <ushanthika0506@gmail.com>
2024-12-15 13:48:31 +0530

    #4, Creare Java Script file

---
+++ client/script.js
@@ -0,0 +1,19 @@
+async function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+        localStorage.setItem('token', data.token);
+        alert("Login successful!");
+        // window.location.href = "/client/home.html";
+    } else {
+        alert(data.message);
+    }
+}
